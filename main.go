16429c188acb07f3c7c0c748780ecc71d075d10a
pavanilla <pavanilla457@gmail.com>
2022-02-18 12:28:13 +0530

    first Commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func sleeping(ch chan int) {
+	fmt.Println(<-ch)
+	fmt.Println("dummy")
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+
+	go func() {
+
+		fmt.Println(1)
+		defer wg.Done()
+	}()
+	go func() {
+		fmt.Println((2))
+		defer wg.Done()
+	}()
+	go func() {
+		fmt.Println(3)
+		defer wg.Done()
+	}()
+
+	wg.Wait()
+	fmt.Println("executin all the go routines")
+
+}
